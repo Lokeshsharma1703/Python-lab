271e551dfea93dfb67ed6ef6cdb76cf33a5f9bfe
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-17 02:05:56 -0700

    Leap year program using nested

---
+++ leap year.py
@@ -0,0 +1,11 @@
+y=int(input('Enter year : '))
+if y%4==0 and y%100!=0:
+    print('Leap year')
+elif y%100==0:
+    if y%400==0:
+        print('Leap year')
+    else:
+        print('Not leap year')
+else:
+    print('Not leap year')
+        
