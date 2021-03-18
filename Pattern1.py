4bc38730aa2bbe690972a8a22e903df92f8f837e
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-18 17:40:38 +0530

    pattern1.py

--- Pattern1.py
+++ Pattern1.py
@@ -2,6 +2,6 @@ n=eval(input('Enter the no. of lines : '))
 for i in range(1,n+1):
     ch=65
     for j in range(1,i+1):
-        print(che(ch),end=' ')
+        print(chr(ch),end=' ')
         ch+=1
     print()
