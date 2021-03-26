a9be0098eb1469d4deee1484750548d01b7f8a48
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-25 21:56:51 -0700

    power of a number n^m

---
+++ 6 power.py
@@ -0,0 +1,5 @@
+''' Program to input two integer values and calculate first number raised to the power second 
+number. '''
+a=int(input('Enter a number : '))
+b=int(input('Enter power : '))
+print('A^n = ',a**b)
