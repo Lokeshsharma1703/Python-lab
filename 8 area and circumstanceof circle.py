bece3672f0dc8f5ed91384a54ccc7d720055f17d
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-25 21:58:01 -0700

    Area of circle

---
+++ 8 area and circumstanceof circle.py
@@ -0,0 +1,5 @@
+#Program to find the area and circumference of a circle, when the radius is entered by the user.
+#However, the user can input radius in integer or float.
+r=eval(input('Enter the radius of a circle : '))
+print('Area of circle is : ',3.14*r**2)
+print('Circumstance of circle : ',2*3.14*r)
