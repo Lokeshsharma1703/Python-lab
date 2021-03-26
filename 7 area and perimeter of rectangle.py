f24a066856de162aa358fce10aa6a827daf2317a
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-25 21:57:30 -0700

    Area and perimeter of rectangle

---
+++ 7 area and perimeter of rectangle.py
@@ -0,0 +1,5 @@
+#Program to find the area and perimeter of a rectangle, when the required input (Length and Breadth) are entered by the user.
+l=eval(input('Enter the length of a Rectangle : '))
+b=eval(input('Enter the breadth of a Rectangle : '))
+print('Area of Rectangle is : ',l*b)
+print('Perimeter of Rectangle is : ',2*(l+b))
