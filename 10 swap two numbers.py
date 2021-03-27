e45db67cdb903e082dbde4eeb2a6566cad645682
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-03-27 18:35:44 +0530

    Update 10 swap two numbers.py

--- 10 swap two numbers.py
+++ 10 swap two numbers.py
@@ -1,5 +1,4 @@
 # Program to input two numbers and print the swapped values of them.
-a=eval(input('A = '))
-b=eval(input('B = '))
+a,b=map(eval,input('Enter the values of A and B respectively : ').split())
 a,b=b,a
 print(f'A = {a}\nB = {b}')
